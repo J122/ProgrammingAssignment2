09614a0f210b9fef9eed70e24792cddf24624541
J122 <f.p.putman@gmail.com>
2015-05-24 08:32:31 +0200

    Created makeCacheMatrix function and cacheSolve fuction
    makeCacheMatrix: This function creates a special "matrix" object that
    can cache its inverse.
    cacheSolve: This function computes the inverse of the special "matrix"
    returned by makeCacheMatrix above. If the inverse has already been
    calculated (and the matrix has not changed), then the cachesolve should
    retrieve the inverse from the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## the below function creates a matrix object that can cache the input matrix and its reverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL # provides a default if cacheSolve has not yet been used
+        set <- function(y) { # set the value of the matrix
+                x <<- y  # caches the inputed matrix so that cacheSolve can check whether it has changed
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get, # creates a list to contain the four functions
+                setmatrix = setmatrix,
+                getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## calls functions stored in the special "matrix" returned by makeCacheMatrix (above). If the inverse has already been calculated,
+## then cacheSolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix() # if an inverse has already been calculated this gets it
+        if(!is.null(m)){ # check to see if cacheSolve has been run before
+                message("getting cached data")
+                return(m)
+        }
+        y <- x$get() # run the getmatrix function to get the value of the input matrix
+        m <- solve(y, ...) # compute the value of the inverse of the input matrix
+        x$setmatrix(m)
+        m # return the inverse
 }
